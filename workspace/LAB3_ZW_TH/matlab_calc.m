3758a54abd768a806f9492a98929084ab3e18ad6
Wang <zew2@illinois.edu>
2023-02-16 16:33:17 -0600

    lab3

---
+++ workspace/LAB3_ZW_TH/matlab_calc.m
@@ -0,0 +1,11 @@
+positive_u = [2.37, 4.78, 5.63, 8.89, 10.0];
+positive_v = [0.27, 1.45, 1.74, 3.27, 3.83];
+
+negative_u = [-2.07, -4.21, -6.14, -8.16, -10.0];
+negative_v = [-0.105, -1.07, -1.96, -2.88, -3.93];
+
+figure;
+% plot(positive_v, positive_u);
+plot(negative_v, negative_u);
+grid on;
+
